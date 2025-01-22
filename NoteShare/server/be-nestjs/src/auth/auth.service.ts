81ce24d157dd178938bbd430fc6dee94c97b0a8c
Sakate Kataro <IS14professional@hotmail.com>
2025-01-22 21:56:37 +0700

    add module auth

---
+++ NoteShare/server/be-nestjs/src/auth/auth.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './entities/user.entity';
+import * as bcrypt from 'bcrypt';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    @InjectRepository(User) private userRepository: Repository<User>,
+    private jwtService: JwtService,
+  ) {}
+
+  async register(email: string, password: string): Promise<User> {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = this.userRepository.create({ email, password: hashedPassword });
+    return this.userRepository.save(user);
+  }
+
+  async validateUser(email: string, password: string): Promise<User | null> {
+    const user = await this.userRepository.findOne({ where: { email } });
+    if (user && (await bcrypt.compare(password, user.password))) {
+      return user;
+    }
+    return null;
+  }
+
+  generateToken(userId: number): string {
+    return this.jwtService.sign({ id: userId });
+  }
+
+  async getUserById(userId: number): Promise<User | null> {
+    return this.userRepository.findOne({ where: { id: userId } });
+  }
+}
